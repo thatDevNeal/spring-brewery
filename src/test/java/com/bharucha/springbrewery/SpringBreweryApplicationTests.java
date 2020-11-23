96a7c4dac456db294161e5791c2457de9755bdbd
Neal Bharucha <nealbharucha@yahoo.com>
2020-11-23 16:59:44 -0600

    First commit, Create Project
    Spring Initializer using IntelliJ Ultimate. Created project

---
+++ src/test/java/com/bharucha/springbrewery/SpringBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharucha.springbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
